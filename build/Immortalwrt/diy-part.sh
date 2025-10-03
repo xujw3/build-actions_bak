d7a053af42d44bc0e68caf7580fac8460e80cd08
xujw3 <kingxujw3@gmail.com>
2025-10-03 16:23:21 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -4,6 +4,7 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 # 后台IP设置
 export Ipv4_ipaddr="10.1.1.2"            # 修改openwrt后台地址(填0为关闭)
